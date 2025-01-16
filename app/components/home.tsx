24f33d6d4dcef7b1718c9a500472e6a996369e3b
enochagbloe <enochagbloe16@gmail.com>
2025-01-16 00:27:48 +0000

    added a database

---
+++ app/components/home.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+function House() {
+    console.log('Am home right?')
+    return ( 
+        <div>
+            <h1>welcome to home page</h1>
+        </div>
+     );
+}
+
+export default House;
